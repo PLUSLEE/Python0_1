a098f520939bcff653ec9852c5642e5ce0a040f1
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-16 16:00:19 +0800

    删除数据点的轮廓，自定义颜色，使用颜色映射，自动保存图表

--- DataVisualization/scatter_squares.py
+++ DataVisualization/scatter_squares.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=5)
+# plt.scatter(x_values, y_values, c='red', edgecolors='none', s=5)  # 删除数据点轮廓,设置数据点颜色
+# plt.scatter(x_values, y_values, c=(1, 0, 0.8), edgecolors='none', s=5)  # 删除数据点轮廓,使用RGB设置数据点颜色
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none',
+            s=40)  # c设置为y值列表，使用cmap告诉pyplot使用哪个颜色映射
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Nubers", fontsize=24)
@@ -22,4 +25,5 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 # 设置每个坐标轴的取值范围
 plt.axis([0, 1100, 0, 1100000])
 
+plt.savefig('squares_plot.png', bbox_inches="tight")  # 参数二，裁减掉空白区域，这句代码放在plt.show()前面，否则不能正常保存图像
 plt.show()
