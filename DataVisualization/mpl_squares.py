4b527a852482779d598cf7d66b519bb1667a0ff2
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-16 15:31:18 +0800

    校正图形

--- DataVisualization/mpl_squares.py
+++ DataVisualization/mpl_squares.py
@@ -2,8 +2,9 @@
 
 import matplotlib.pyplot as plt  # as为指定别名，避免反复输入pyplot
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # 设置图标标题，并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
