3fc033862b67a24a2e042d40236810a78caeca14
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-18 14:16:58 +0800

    随机漫步-选择方向

--- DataVisualization/random_walk.py
+++ DataVisualization/random_walk.py
@@ -13,3 +13,28 @@ class RandomWalk():
         # 所有随机漫步都始于（0,0）
         self.x_value = [0]
         self.y_value = [0]
+
+    def fill_walk(self):
+        """计算随机漫步所包含的所有点"""
+
+        # 不断漫步，直到列表达到指定长度
+        while len(self.x_value) < self.num_points:
+            # 决定前进方向以及沿这个方向前进的距离
+            x_direction = choice([1, -1])#向右为1，向左为-1
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # 拒绝原地踏步
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # 计算下一个点的x和y值
+            next_x = self.x_value[-1] + x_step
+            next_y = self.y_value[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
