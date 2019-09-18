02342b75238fa7ef140d3c376576bfd8eb958286
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-18 14:24:10 +0800

    模拟多次随机漫步图

--- DataVisualization/rw_visual.py
+++ DataVisualization/rw_visual.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 
 from DataVisualization.random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_value, rw.y_value, s=15)
-plt.show()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_value, rw.y_value, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n):")
+    if keep_running == 'n':
+        break
