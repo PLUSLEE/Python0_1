e7aa1933360f1d11aeb7a772cb7d052c29441b55
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-18 14:32:47 +0800

    随机漫步图进行着色处理

--- DataVisualization/rw_visual.py
+++ DataVisualization/rw_visual.py
@@ -7,7 +7,9 @@ from DataVisualization.random_walk import RandomWalk
 while True:
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_value, rw.y_value, s=15)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_value, rw.y_value, c=point_numbers, cmap=plt.cm.Reds, edgecolors='none', s=6)
+    # plt.scatter(rw.x_value, rw.y_value, s=15)
     plt.show()
 
     keep_running = input("Make another walk?(y/n):")
