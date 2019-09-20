d3488c803a30585089740d4ea360caba7ad8bbbe
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-20 17:07:29 +0800

    再绘制一个数据系列

--- downloadData/highs_lows_2.py
+++ downloadData/highs_lows_2.py
@@ -4,7 +4,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-# 从文件中获取最高气温
+# 从文件中获取日期、最高气温、最低气温
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -14,7 +14,7 @@ with open(filename) as f:
     for index, column_header in enumerate(header_row):  # 获取每个元素的索引和值
         print(index, column_header)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -22,18 +22,22 @@ with open(filename) as f:
         high = int(row[1])  # 读取csv对应index的值，同时将字符串转换为数字int
         highs.append(high)
 
+        low = int(row[3])
+        lows.append(low)
+
     print(highs)
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=300, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # 设置图形格式
-plt.title("Daily high temperatures, 2014", fontsize=24)
+plt.title("Daily high and low temperatures, 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 plt.ylabel("Temperature (F)", fontsize=16)
 fig.autofmt_xdate()
 plt.tick_params(axis='both', which='major', labelsize=16)
 
-plt.savefig('high_low_2.png', bbox_inches="tight")
+plt.savefig('high_low_3.png', bbox_inches="tight")
 plt.show()
