9b32bfce118f7fe8d889c052fed99af9752d9dbc
ledtravel <LZWORKER@OUTLOOK.COM>
2019-09-20 16:00:25 +0800

    打印文件头和其位置

--- downloadData/highs_lows.py
+++ downloadData/highs_lows.py
@@ -6,4 +6,7 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)  # 调用read一次，只读取一行csv
-    print(header_row)
+    # print(header_row)
+
+    for index, column_header in enumerate(header_row):  # 获取每个元素的索引和值
+        print(index, column_header)
